7d4f40a465c5c5aa9ce3799d372a6a869eb0aef0
Your Name <suhasmashety@gmail.com>
2025-09-05 06:35:05 +0000

    init

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -7,8 +7,11 @@ pub mod vault {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+
         msg!("Greetings from: {:?}", ctx.program_id);
-        msg!("Greetings from: {:?}", ctx.program_id);
+
+
+        
         Ok(())
     }
 }
